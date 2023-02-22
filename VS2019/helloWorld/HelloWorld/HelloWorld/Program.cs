8e06556d9a23a9df712b0515f34121c33ea4816b
Anh Nguyen <112703772+anhnt184@users.noreply.github.com>
2023-02-22 22:14:05 +0200

    First program with input variable

---
+++ VS2019/helloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char tab = '\t';
+            char backSpace = '\b';
+            String name="";
+            Console.WriteLine("Hei maailma!");
+            Console.WriteLine("Minä olen Anh");
+            String message = "Minä olen ";
+            int age = 39;
+            Console.WriteLine(tab + message +  backSpace + age + "-voutias.");
+            Console.Write("Mika sun nimi on? ");
+            name = Console.ReadLine();
+            while (name == "")
+            {
+                Console.Write("Voitko ystävällisesti kirjoittaa nimesi uudelleen? ");
+                name = Console.ReadLine();
+                if (name!= "")
+                {
+                    Console.WriteLine("Moi " + name);
+                    break;
+                }
+            }
+            
+                
+            
+
+        }
+    }
+}
